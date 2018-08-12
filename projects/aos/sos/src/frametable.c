cd7ad92af09b378be8e7993484ffc0dd6cc9383b
patso21 <sopatrick@outlook.com>
2018-08-12 10:15:11 -0400

    yea its still messed

--- projects/aos/sos/src/frametable.c
+++ projects/aos/sos/src/frametable.c
@@ -41,6 +41,10 @@ static seL4_Word page_num_to_paddr(seL4_Word page){
 
 static seL4_Word frame_table_alloc(seL4_Word* vaddr){
     ut_t *ut = ut_alloc_4k_untyped(&curr_paddr);
+    if(ut == NULL){
+        ZF_LOGE("Out of memory");
+        return -1;
+    }
     seL4_CPtr cap = cspace_alloc_slot(cspace);
     if (cap == seL4_CapNull) {
         ZF_LOGE("Cspace full");
@@ -80,37 +84,48 @@ void frame_table_init(cspace_t *cs) {
     bot_paddr = ut_size();
     */
 
-    //ut_size is huge, run out of cap space
     size_t frame_table_size = ut_size()/PAGE_SIZE_4K;
     size_t frame_table_pages = BYTES_TO_4K_PAGES(frame_table_size * sizeof(struct frame_table_entry));
     seL4_Word frame_table_vaddr;
-    printf("===%d, %d\n\n\n", frame_table_pages, sizeof(struct frame_table_entry));
+    
     for(size_t i = 0; i < frame_table_pages; i++){
         top_paddr = frame_table_alloc(&frame_table_vaddr);
         
         if(i == 0)
-        printf("%d, %d, %lx\n", frame_table_pages, i, frame_table_vaddr);
+        printf("%d, %d, %lx\n", top_paddr, i, frame_table_vaddr);
     }
     //init values (physical memory allocates downwards)
     top_paddr -= PAGE_SIZE_4K; // first page will be here
     bot_paddr = top_paddr - ut_size();
+    printf("===%d, %d, %lx\n\n\n", frame_table_pages, sizeof(struct frame_table_entry), frame_table_size );
     
-    for(int i = top_paddr; i <= bot_paddr; i+= PAGE_SIZE_4K){
-        frame_table[paddr_to_page_num(i)].cap == NULL;
+    //vmfault on some addresses
+    frame_table = (struct frame_table_entry*) frame_table_vaddr;
+    for(size_t i = 0; i <= frame_table_size; i+= 1){
+        //printf("%lx\n" , (frame_table + i));
+        (frame_table + i)->cap = seL4_CapNull;
     }
 
     // test
     assert(frame_table_vaddr);
     printf("frame_table_vaddr: %lx, value %lx\n", frame_table_vaddr, *(seL4_Word *)frame_table_vaddr);
-    frame_table = (struct frame_table_entry*) frame_table_vaddr;
-    *(seL4_Word *) &(frame_table[999]) = 0x37;
-    *(seL4_Word *) &(frame_table[frame_table_size-1]) = 0x37;
-    printf("%lx\n", &(frame_table[frame_table_size+4000] ));
-    *(seL4_Word *) &(frame_table[frame_table_size+999]) = 0x37;
+    
+    //*(seL4_Word *) &(frame_table[0x9000]) = 0x37;
+    //*(seL4_Word *) &(frame_table[0x8aac]) = 0x37;
+    //*(seL4_Word *) &(frame_table[0x8aab]) = 0x37; // this address is fucked
+    
+    //*(seL4_Word *) &(frame_table[999]) = 0x37;
+    //*(seL4_Word *) &(frame_table[frame_table_size-1]) = 0x37;
+    //printf("%lx\n", &(frame_table[frame_table_size+4000] ));
+    //*(seL4_Word *) &(frame_table[frame_table_size+999]) = 0x37;
 }
 
 seL4_Word frame_alloc(seL4_Word *vaddr) {
     ut_t *ut = ut_alloc_4k_untyped(&curr_paddr);
+    if(ut == NULL){
+        ZF_LOGE("Out of memory");
+        return -1;
+    }
     seL4_CPtr cap = cspace_alloc_slot(cspace);
     if (cap == seL4_CapNull) {
         ZF_LOGE("Cspace full");
@@ -142,7 +157,7 @@ seL4_Word frame_alloc(seL4_Word *vaddr) {
 
     
     seL4_Word page_num = (top_paddr - curr_paddr)/PAGE_SIZE_4K;
-    printf("first: %lx, curr:%lx, diff:%lx, maxdiff: %lx, pagenum: %lx, maxpage: %lx\n", top_paddr, curr_paddr, top_paddr-curr_paddr,top_paddr-bot_paddr, page_num, paddr_to_page_num(bot_paddr));
+    //printf("first: %lx, curr:%lx, diff:%lx, maxdiff: %lx, pagenum: %lx, maxpage: %lx\n", top_paddr, curr_paddr, top_paddr-curr_paddr,top_paddr-bot_paddr, page_num, paddr_to_page_num(bot_paddr));
     frame_table[page_num].cap = cap;
     frame_table[page_num].untyped = ut;
     return page_num;
@@ -156,13 +171,13 @@ void frame_free(seL4_Word page) {
         ZF_LOGE("Page does not exist");
         return;
     }
-    if(frame_table[page].cap == NULL){
+    if(frame_table[page].cap == seL4_CapNull){
         ZF_LOGE("Page is already free");
         return;
     }
     cspace_delete(cspace, frame_table[page].cap);
     cspace_free_slot(cspace, frame_table[page].cap);
     ut_free(frame_table[page].untyped, PAGE_BITS_4K);
-    frame_table[page].cap == NULL;
+    frame_table[page].cap = seL4_CapNull;
 }
 
