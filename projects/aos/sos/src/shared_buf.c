0e9cf604aaf2890ce6040d3d9d82bacbaa5fe4aa
patso21 <sopatrick@outlook.com>
2018-09-01 03:31:54 -0400

    added a shared buffer

---
+++ projects/aos/sos/src/shared_buf.c
@@ -0,0 +1,44 @@
+#include "shared_buf.h"
+#include "vmem_layout.h"
+
+void * shared_buf;
+
+void shared_buf_init(){
+    seL4_Word first_frame;
+    frame_alloc(&first_frame);
+    seL4_Word vaddr;
+    for(size_t i = 1; i < SHARED_BUF_PAGES; i++){
+        frame_alloc(&vaddr);
+    }
+    assert(first_frame);
+    assert(vaddr);
+    shared_buf = (void *) first_frame;
+}
+
+static void check_len(size_t * len){
+    if(*len > PAGE_SIZE_4K * SHARED_BUF_PAGES){
+        *len = PAGE_SIZE_4K * SHARED_BUF_PAGES;
+    }
+}
+
+size_t user_copyin(seL4_Word user_vaddr, size_t len){
+    check_len(&len);
+    memcpy(PROCESS_SHARED_BUF_TOP - PAGE_SIZE_4K * SHARED_BUF_PAGES, user_vaddr, len);
+    return len;
+}
+size_t user_copyout(seL4_Word user_vaddr, size_t len){
+    check_len(&len);
+    memcpy(user_vaddr, PROCESS_SHARED_BUF_TOP - PAGE_SIZE_4K * SHARED_BUF_PAGES, len);
+    return len;
+}
+
+size_t sos_copyin(seL4_Word kernel_vaddr, size_t len){
+    check_len(&len);
+    memcpy(shared_buf, kernel_vaddr, len);
+    return len;
+}
+size_t sos_copyout(seL4_Word kernel_vaddr, size_t len){
+    check_len(&len);
+    memcpy(kernel_vaddr, shared_buf, len);
+    return len;
+}
