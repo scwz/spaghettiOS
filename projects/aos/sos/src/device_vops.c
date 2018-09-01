f5624d631e9f76a3b576771cd6943f47790bd52e
patso21 <sopatrick@outlook.com>
2018-09-01 06:15:08 -0400

    added more vnode stubs

---
+++ projects/aos/sos/src/device_vops.c
@@ -0,0 +1,91 @@
+#include "vfs.h"
+#include "vnode.h"
+
+static int dev_eachopen(struct vnode *v, int flags)
+{
+	struct device *d = v->vn_data;
+
+    //add make handler stuff here?
+	return 0;
+}
+
+static
+int
+dev_write(struct vnode *v, struct uio *uio)
+{
+	struct device *d = v->vn_data;
+	int result;
+
+	//just write here
+
+	return 0;
+}
+
+static
+int
+dev_read(struct vnode *v, struct uio *uio)
+{
+	struct device *d = v->vn_data;
+	int result;
+    // do the read here
+	return 0;
+}
+
+static int dev_getdirent(){
+    return 0;
+}
+
+static int dev_stat(){
+    return 0;
+}
+
+static int dev_lookup(){
+    return 0;
+}
+
+static int dev_lookparent(){
+    return 0;
+}
+
+static const struct vnode_ops dev_vnode_ops = {
+	.vop_magic = VOP_MAGIC,
+
+	.vop_eachopen = dev_eachopen,
+
+	.vop_read = dev_read,
+    .vop_getdirentry = dev_getdirent,
+
+	.vop_write = dev_write,
+	
+	.vop_stat = dev_stat,
+	.vop_lookup = dev_lookup,
+	.vop_lookparent = dev_lookparent,
+};
+
+struct vnode *
+dev_create_vnode()
+{
+	int result;
+	struct vnode *v;
+
+	v = malloc(sizeof(struct vnode));
+	if (v==NULL) {
+		return NULL;
+	}
+
+	result = vnode_init(v, &dev_vnode_ops,  NULL);
+	if (result != 0) {
+		ZF_LOGV("While creating vnode for device: vnode_init: %s\n",
+		      strerror(result));
+	}
+
+	return v;
+}
+
+void
+dev_uncreate_vnode(struct vnode *vn)
+{
+	assert(vn->vn_ops == &dev_vnode_ops);
+	vnode_cleanup(vn);
+	free(vn);
+}
--- projects/aos/sos/src/vfs.c
+++ projects/aos/sos/src/vfs.c
@@ -1,20 +1,68 @@
 #include "vfs.h"
 #include "vnode.h"
+#include "string.h"
 
-int vfs_lookup(char *path, struct vnode **result) {
-    return 0;
+int vfs_lookup(char *path, struct vnode **retval) {
+    struct vnode *startvn;
+	int result;
+
+    // idk this does; i think if console no path
+	if(strlen(path) == 0){
+        *retval = startvn;
+        return 0;
+    }
+
+
+	result = VOP_LOOKUP(startvn, path, retval);
+
+	VOP_DECREF(startvn);
+    return result;
 }
 
-int vfs_lookparent(char *path, struct vnode **result, char *buf, size_t buflen) {
-    return 0;
+int vfs_lookparent(char *path, struct vnode **retval, char *buf, size_t buflen) {
+    struct vnode *startvn;
+	int result;
+
+    // idk this does; i think if console no path
+	if(strlen(path) == 0){
+        *retval = startvn;
+        return 0;
+    }
+
+
+	result = VOP_LOOKPARENT(startvn, path, retval, buf, buflen);
+
+	VOP_DECREF(startvn);
 }
 
 int vfs_open(char *path, int openflags, int mode, struct vnode **ret) {
-    return 0;
+    int how;
+	int result;
+	int canwrite;
+	struct vnode *vn = NULL;
+
+    
+	result = vfs_lookup(path, &vn);
+
+	if (result) {
+		return result;
+	}
+
+	assert(vn != NULL);
+
+	result = VOP_EACHOPEN(vn, openflags);
+	if (result) {
+		VOP_DECREF(vn);
+		return result;
+	}
+
+	*ret = vn;
+
+return 0;
 }
 
 void vfs_close(struct vnode *vn) {
-
+    VOP_DECREF(vn);
 }
 
 void vfs_bootstrap(void) {
