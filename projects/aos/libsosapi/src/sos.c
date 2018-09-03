a67feeb73cfde6dc3a5f2b2360439dea0672d70a
william <wmchan622@gmail.com>
2018-09-03 17:21:20 +1000

    somewhat implement close on the client side

--- projects/aos/libsosapi/src/sos.c
+++ projects/aos/libsosapi/src/sos.c
@@ -39,80 +39,94 @@ static size_t user_copyout(void* user_vaddr, size_t len){
 
 int sos_sys_open(const char *path, fmode_t mode)
 {
-    seL4_MessageInfo_t tag;
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, strlen(path) + 2);
     seL4_SetMR(0, SOS_SYS_OPEN);
     seL4_SetMR(1, mode);
     strcpy(seL4_GetIPCBuffer()->msg + 2, path);
-    tag = seL4_MessageInfo_new(0, 0, 0, strlen(path) + 2);
     seL4_Call(SOS_IPC_EP_CAP, tag);
+
     if(seL4_GetMR(0)){
         return -1;
     }
+
     return seL4_GetMR(1);
 }
 
 int sos_sys_close(int file)  {
-    return -1;
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 2);
+    seL4_SetMR(0, SOS_SYS_CLOSE);
+    seL4_SetMR(1, file);
+    seL4_Call(SOS_IPC_EP_CAP, tag);
+
+    return 0;
 }
 
 int sos_sys_read(int file, char *buf, size_t nbyte)
 {   
-    seL4_MessageInfo_t tag;
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 3);
     seL4_SetMR(0, SOS_SYS_READ);
     seL4_SetMR(1, file);
     seL4_SetMR(2, nbyte);
-    tag = seL4_MessageInfo_new(0, 0, 0, 3);
     seL4_Call(SOS_IPC_EP_CAP, tag);
+
     size_t bytes_read = seL4_GetMR(0);
     user_copyout(buf, bytes_read);
+
     return bytes_read;
 }
 
 int sos_sys_write(int file, const char *buf, size_t nbyte)
 {
-    seL4_MessageInfo_t tag;
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 3);
     seL4_SetMR(0, SOS_SYS_WRITE);
     nbyte = user_copyin(buf, nbyte);
     seL4_SetMR(1, file);
     seL4_SetMR(2, nbyte);
-    tag = seL4_MessageInfo_new(0, 0, 0, 3);
     seL4_Call(SOS_IPC_EP_CAP, tag);
+
     return seL4_GetMR(0);
 }
 
 int sos_getdirent(int pos, char *name, size_t nbyte)
 {
+    assert(!"sos_getdirent not implemented!");
     return -1;
 }
 
 int sos_stat(const char *path, sos_stat_t *buf)
 {
+    assert(!"sos_stat not implemented!");
     return -1;
 }
 
 pid_t sos_process_create(const char *path)
 {
+    assert(!"sos_process_create not implemented!");
     return -1;
 }
 
 int sos_process_delete(pid_t pid)
 {
+    assert(!"sos_process_delete not implemented!");
     return -1;
 }
 
 pid_t sos_my_id(void)
 {
+    assert(!"sos_my_id not implemented!");
     return -1;
 
 }
 
 int sos_process_status(sos_process_t *processes, unsigned max)
 {
+    assert(!"sos_process_status not implemented!");
     return -1;
 }
 
 pid_t sos_process_wait(pid_t pid)
 {
+    assert(!"sos_process_wait not implemented!");
     return -1;
 
 }
