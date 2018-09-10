8ba9cce0a293deabd5d869c6114f52cf6fac87b1
william <wmchan622@gmail.com>
2018-09-11 01:52:40 +1000

    remove text

--- projects/aos/apps/sosh/src/sosh.c
+++ projects/aos/apps/sosh/src/sosh.c
@@ -397,7 +397,6 @@ int main(void)
             fflush(stdout);
             r = read(in, bp, BUF_SZ - 1 + buf - bp);
             
-                    printf("Executing... %d\n", r);
             if (r < 0) {
                 printf("Console read failed!\n");
                 done = 1;
