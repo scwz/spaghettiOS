ec5c9b49a2ef46bf37915214332e32acafb1c3fc
patso21 <sopatrick@outlook.com>
2018-10-13 20:00:01 -0400

    fix sharedbuf warnings

--- projects/aos/sos/src/vm/shared_buf.c
+++ projects/aos/sos/src/vm/shared_buf.c
@@ -26,7 +26,7 @@ void sos_map_buf(pid_t pid){
     struct proc *curproc = proc_get(pid);
     struct region* reg = as_seek_region(curproc->as, (seL4_Word) PROCESS_SHARED_BUF_TOP);
     for(size_t i = 0; i < SHARED_BUF_PAGES; i++){
-        seL4_Word page = vaddr_to_page_num(shared_buf + i*PAGE_SIZE_4K);
+        seL4_Word page = vaddr_to_page_num((seL4_Word)shared_buf + i*PAGE_SIZE_4K);
         struct frame_table_entry * fte = get_frame(page);
         seL4_CPtr slot = cspace_alloc_slot(cs);
         cspace_copy(cs, slot, cs, fte->cap, seL4_AllRights);
@@ -44,11 +44,11 @@ static void check_len(size_t * len){
 
 size_t sos_copyin(seL4_Word kernel_vaddr, size_t len){
     check_len(&len);
-    memcpy(shared_buf, kernel_vaddr, len);
+    memcpy((void*) shared_buf, (void*) kernel_vaddr, len);
     return len;
 }
 size_t sos_copyout(seL4_Word kernel_vaddr, size_t len){
     check_len(&len);
-    memcpy(kernel_vaddr, shared_buf, len);
+    memcpy((void*) kernel_vaddr, (void*) shared_buf, len);
     return len;
 }
